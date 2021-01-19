253c6c120247cc21cee96117cd75a05a21082f84
Mark <canedam2016@gmail.com>
2021-01-19 18:28:57 +0800

    Added docstrings to config classes

--- data_pipeline/plugins/helpers/configs/mongo/mongo_config.py
+++ data_pipeline/plugins/helpers/configs/mongo/mongo_config.py
@@ -2,6 +2,7 @@
 
 
 class MongoConfig:
+    """Class abstraction for setting up MongoDB configuration."""
     def __init__(self, conn_id: str, collection: str, db: str = ''):
         self.conn_id = conn_id
         self.db = db
--- data_pipeline/plugins/helpers/configs/postgres/postgres_config.py
+++ data_pipeline/plugins/helpers/configs/postgres/postgres_config.py
@@ -2,6 +2,7 @@
 
 
 class PostgresConfig:
+    """Class abstraction for setting up PostgreSQL configuration."""
     def __init__(self, conn_id: str, table: str = '', db: str = ''):
         self.conn_id = conn_id
         self.db = db
